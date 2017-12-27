c1ed6c53f92d753dad8d04c6d516f92423a65ea2
love3forever <wm_gaming@163.com>
2017-12-27 11:50:12 +0800

    添加dialog效果 增加echarts

--- src/containers/leftContainer.js
+++ src/containers/leftContainer.js
@@ -3,10 +3,14 @@ import Paper from 'material-ui/Paper'
 import Menu from 'material-ui/Menu'
 import MenuItem from 'material-ui/MenuItem'
 import IconButton from 'material-ui/IconButton'
+import FlatButton from 'material-ui/FlatButton'
 import RemoveRedEye from 'material-ui/svg-icons/image/remove-red-eye'
 import NavigationClose from 'material-ui/svg-icons/navigation/close'
 import Drawer from 'material-ui/Drawer'
 import AppBar from 'material-ui/AppBar'
+import Dialog from 'material-ui/Dialog'
+
+import ReactEcharts from 'echarts-for-react'
 
 const style = {
     div: {
@@ -35,32 +39,121 @@ const style = {
     }
 }
 
+const option = {
+    title: {
+        text: '饼图程序调用高亮示例',
+        x: 'center'
+    },
+    tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+    },
+    series: [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: [
+                '50%', '60%'
+            ],
+            data: [
+                {
+                    value: 335,
+                    name: '直接访问'
+                }, {
+                    value: 310,
+                    name: '邮件营销'
+                }, {
+                    value: 234,
+                    name: '联盟广告'
+                }, {
+                    value: 135,
+                    name: '视频广告'
+                }, {
+                    value: 1548,
+                    name: '搜索引擎'
+                }
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+}
+
 class LeftContainer extends Component {
     constructor(props) {
         super(props)
-        this.state = {open:false}
+        this.state = {
+            drawerOpen:false,
+            dialogOpen:false
+        }
     }
 
-    handleToggle = () => this.setState({open:!this.state.open})
+    handleDrawerToggle = () => this.setState({drawerOpen:!this.state.drawerOpen})
+
+    handleOpen = () => {
+        this.setState({dialogOpen: true});
+    };
+
+    handleClose = () => {
+        this.setState({dialogOpen: false});
+    };
 
     render() {
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <FlatButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleClose}
+            />,
+            ];
+
+
         return (
             <div style={style.div}>
                 <div style={style.div}>
                     <Paper id="left-menu" style={style.paper}>
                         <Menu autoWidth={false} style={style.menu} menuItemStyle={style.menulist}>
-                            <MenuItem style={style.item} innerDivStyle={style.menulist} onClick={this.handleToggle}><RemoveRedEye style={style.icon}/></MenuItem>
-                            <MenuItem style={style.item} innerDivStyle={style.menulist}><RemoveRedEye style={style.icon}/></MenuItem>
+                            <MenuItem style={style.item} innerDivStyle={style.menulist} onClick={this.handleDrawerToggle}><RemoveRedEye style={style.icon}/></MenuItem>
+                            <MenuItem style={style.item} innerDivStyle={style.menulist} onClick={this.handleOpen}><RemoveRedEye style={style.icon}/></MenuItem>
                             <MenuItem style={style.item} innerDivStyle={style.menulist}><RemoveRedEye style={style.icon}/></MenuItem>
                             <MenuItem style={style.item} innerDivStyle={style.menulist}><RemoveRedEye style={style.icon}/></MenuItem>
                         </Menu>
                     </Paper>
                 </div>
-                <Drawer open={this.state.open}>
-                    <AppBar title="AppBar" iconElementRight={<IconButton onClick={this.handleToggle}><NavigationClose /></IconButton>}/>
+                <Drawer open={this.state.drawerOpen}>
+                    <AppBar title="AppBar" iconElementRight={<IconButton onClick={this.handleDrawerToggle}><NavigationClose /></IconButton>}/>
                     <MenuItem>Menu Item</MenuItem>
                     <MenuItem>Menu Item 2</MenuItem>
                 </Drawer>
+                <Dialog
+                    title="Dialog With Actions"
+                    modal={false}
+                    actions={actions}
+                    open={this.state.dialogOpen}
+                    onRequestClose={this.handleClose}
+                    >
+                    <ReactEcharts
+                        option={option}
+                        style={{height: '300px', width: '100%'}}
+                    />
+                </Dialog>
             </div>
         );
     }
